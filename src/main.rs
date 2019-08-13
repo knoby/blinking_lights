3e380aa2ea3b67fb0e875f45c6054a0430fd8059
KnobY <maximilian.brinkmann@posteo.de>
2019-08-13 22:00:53 +0200

    Blinking with IRQ and DMA

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,10 @@ mod ws2812;
 const APP: () = {
     static mut ON_BOARD_LED: stm32f1xx_hal::gpio::gpioa::PA5<Output<PushPull>> = ();
     static mut TIMER: ws2812::PwmPatternDB = ();
+    static mut DMA_BUFFER: [u16; 48] = [0; 48];
 
-    #[init(schedule=[cyclic])]
-    fn init(c: init::Context) -> init::LateResources {
+    #[init(schedule=[cyclic], resources=[DMA_BUFFER])]
+    fn init(mut c: init::Context) -> init::LateResources {
         // Freeze clock frequencies
         let mut flash = c.device.FLASH.constrain();
         let mut rcc = c.device.RCC.constrain();
@@ -33,16 +34,18 @@ const APP: () = {
 
         // Setup On Board LED
         let mut gpioa = c.device.GPIOA.split(&mut rcc.apb2);
-        let led = gpioa.pa5.into_push_pull_output(&mut gpioa.crl);
+        let mut led = gpioa.pa5.into_push_pull_output(&mut gpioa.crl);
 
         // Setup the PWM Output
         let tim2 = stm32f1xx_hal::timer::Timer::tim2(c.device.TIM2, 1.hz(), clocks, &mut rcc.apb1);
         let dma1_c2 = c.device.DMA1.split(&mut rcc.ahb).2;
-        let ws2812 = ws2812::PwmPatternDB::new(tim2, dma1_c2, clocks);
+        let mut ws2812 = ws2812::PwmPatternDB::new(tim2, dma1_c2, clocks);
 
         c.schedule
             .cyclic(rtfm::Instant::now() + 32_000_000.cycles())
             .unwrap();
+
+        ws2812.start(&mut c.resources.DMA_BUFFER);
 
         init::LateResources {
             ON_BOARD_LED: led,
@@ -65,10 +68,21 @@ const APP: () = {
     }
 
     #[interrupt( resources = [ON_BOARD_LED, TIMER])]
-    fn TIM2(c: TIM2::Context) {
-        #[allow(deprecated)]
-        c.resources.ON_BOARD_LED.toggle();
-        c.resources.TIMER.reset_isr();
+    fn TIM2(mut c: TIM2::Context) {
+        if c.resources.TIMER.is_cmp_irq() {
+            #[allow(deprecated)]
+            c.resources.ON_BOARD_LED.set_low();
+        } else {
+            #[allow(deprecated)]
+            c.resources.ON_BOARD_LED.set_high();
+        }
+
+        c.resources.TIMER.reset_isr_tim();
+    }
+
+    #[interrupt( resources = [TIMER])]
+    fn DMA1_CHANNEL2(mut c: DMA1_CHANNEL2::Context) {
+        c.resources.TIMER.reset_isr_dma();
     }
 
     extern "C" {
--- src/ws2812/mod.rs
+++ src/ws2812/mod.rs
@@ -6,20 +6,22 @@ use stm32f1xx_hal::timer::PclkSrc;
 pub struct PwmPatternDB {
     tim: stm32f1xx_hal::device::TIM2,
     dma_ch: stm32f1xx_hal::dma::dma1::C2,
-    clocks: stm32f1xx_hal::rcc::Clocks,
+    //clocks: stm32f1xx_hal::rcc::Clocks,
 }
 
 impl PwmPatternDB {
     pub fn new(
         tim: stm32f1xx_hal::timer::Timer<stm32f1xx_hal::device::TIM2>,
-        dma_ch: stm32f1xx_hal::dma::dma1::C2,
+        mut dma_ch: stm32f1xx_hal::dma::dma1::C2,
         clocks: stm32f1xx_hal::rcc::Clocks,
     ) -> Self {
         // Get the internal registers of the timer
         let tim = tim.release();
 
         // Calculate the timer prescaler
-        let frequency = 1.hz();
+        //let frequency = 800.khz(); // T = 1.25u --> Length for WS2812
+        let frequency = 2.hz(); // Testing Frequency to visulise LED
+
         let timer_clock = stm32f1xx_hal::device::TIM2::get_clk(&clocks);
 
         let ticks = timer_clock.0 / frequency.0;
@@ -30,31 +32,92 @@ impl PwmPatternDB {
 
         tim.arr.write(|w| unsafe { w.bits(u32(arr)) });
 
-        let ccr = arr / 5;
-        tim.ccr1.write(|w| w.ccr().bits(ccr));
+        tim.ccr1.write(|w| w.ccr().bits(arr / 2));
 
         // Trigger an update event to load the prescaler value to the clock
         tim.egr.write(|w| w.ug().set_bit());
+
         // The above line raises an update event which will indicate
         // that the timer is already finished. Since this is not the case,
         // it should be cleared
         tim.sr.modify(|_, w| w.uif().clear_bit());
 
-        // Enable the Interrupt
-        tim.dier.write(|w| w.uie().set_bit().cc1ie().set_bit());
+        // Enable the Interrupt on Compare and Update and DMA on Update
+        tim.dier
+            .write(|w| w.uie().set_bit().cc1ie().set_bit().ude().set_bit());
 
-        // Start the timer
-        tim.cr1.modify(|_, w| w.cen().set_bit());
+        // Configuration of the dma
+
+        // Compare Address Base + Offset Length 16 bit
+        let arr_address = 0x4000_0000 + 0x34;
+        dma_ch.set_peripheral_address(arr_address, false);
+
+        // Set Number of transfers
+        unsafe {
+            dma_ch.ch().ndtr.write(|w| w.bits(48));
+        }
+
+        // Configuration of DMA
+        unsafe {
+            dma_ch.ch().cr.write(|w| {
+                w.msize()
+                    .bits(0x01) // Write 16 bit
+                    .psize()
+                    .bits(0x01) // Write 16 bit
+                    .minc()
+                    .set_bit() // Inc Memocry Add
+                    .circ()
+                    .set_bit() // Enable Circular Mode
+                    .dir()
+                    .set_bit() // Memory -> Per
+                    .htie()
+                    .set_bit() // Enable Half Transfer Interrupt
+                    .tcie()
+                    .set_bit() // Enable Transfer Comple Interrupt
+            });
+        }
 
         PwmPatternDB {
             tim,
             dma_ch,
-            clocks,
+            //clocks,
         }
     }
 
-    pub fn reset_isr(&mut self) {
-        self.tim.sr.modify(|_, w| w.uif().clear_bit());
-        self.tim.sr.modify(|_, w| w.cc1if().clear_bit());
+    pub fn start(&mut self, buffer: &mut [u16; 48]) {
+        for item in buffer.iter_mut().step_by(2) {
+            *item = 40_000;
+        }
+
+        for item in buffer.iter_mut().skip(1).step_by(2) {
+            *item = 5000;
+        }
+        // Set Memory Add
+        self.dma_ch
+            .set_memory_address(buffer as *const _ as u32, true);
+        // Enable DMA
+        self.dma_ch.start();
+        // Start the timer
+        self.tim.cr1.modify(|_, w| w.cen().set_bit());
+    }
+
+    pub fn stop(&mut self) {
+        self.dma_ch.stop();
+    }
+
+    pub fn reset_isr_tim(&mut self) {
+        self.tim
+            .sr
+            .modify(|_, w| w.uif().clear_bit().cc1if().clear_bit());
+    }
+
+    pub fn reset_isr_dma(&mut self) {
+        self.dma_ch
+            .ifcr()
+            .write(|w| w.ctcif2().clear().chtif2().clear());
+    }
+
+    pub fn is_cmp_irq(&self) -> bool {
+        !self.tim.sr.read().uif().is_update_pending()
     }
 }
